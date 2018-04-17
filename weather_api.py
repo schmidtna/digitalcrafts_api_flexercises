f8b4c00ba469d878a30f6a73701eb2ce337505cd
Nick Schmidt <nick@schmidtyanimation.com>
2018-04-16 20:03:18 -0500

    starting weather api

---
+++ weather_api.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+import os
+
+import tornado.ioloop
+import tornado.web
+import tornado.log
+
+import markdown2
+
+import queries
+import requests
+
+from jinja2 import \
+  Environment, PackageLoader, select_autoescape
+
+ENV = Environment(
+  loader=PackageLoader('weather_app', 'templates'),
+  autoescape=select_autoescape(['html', 'xml'])
+)
+
+payload = {}
+citydata = requests.get
+
+class TemplateHandler(tornado.web.RequestHandler):
+  def initialize(self):
+    self.session = queries.Session(
+      'postgresql://postgres@localhost:5432/weather_app')
+
+  def render_template (self, tpl, context):
+    template = ENV.get_template(tpl)
+    self.write(template.render(**context))
+
+class MainHandler(TemplateHandler):
+  def get (self):
+    posts = self.session.query('SELECT * FROM post')
+    self.render_template("home.html", { 'posts': posts })
+
+# class BlogPostHandler(TemplateHandler):
+#   def get (self, slug):
+#     posts = self.session.query(
+#       'SELECT * FROM post WHERE slug = %(slug)s',
+#       {'slug': slug}
+#     )
+#     html = markdown2.markdown(posts[0] ['body'])
+#     context = {
+#       'post' : posts[0],
+#       'html' : html
+#     }
+#     self.render_template("post.html", context)
+
+class SearchHandler(TemplateHandler):
+  def get (self, slug):
+    posts = self.session.query(
+     
+    self.render_template("comment.html", {'post' : posts[0]})
+
+  def post (self, slug):
+    comment = self.get_body_argument('comment')
+    posts = self.session.query(
+      'SELECT * FROM post WHERE slug = %(slug)s',
+      {'slug' : slug}
+    )
+    self.redirect('/post' + slug)
+
+    
+
+    
+
+
+def make_app():
+  return tornado.web.Application([
+    (r"/", MainHandler),
+    (r"/post/(.*)/comment", SearchHandler),
+    (r"/post/(.*)", BlogPostHandler),
+    (r"/static/(.*)", 
+      tornado.web.StaticFileHandler, {'path': 'static'}),
+  ], autoreload=True)
+
+
+if __name__ == "__main__":
+  tornado.log.enable_pretty_logging()
+  app = make_app()
+  app.listen(int(os.environ.get('PORT', '8080')))
+  tornado.ioloop.IOLoop.current().start()
